92a5b13839941f8153528dae4539cb6b8807faec
Brendan Cron <brendancron@gmail.com>
2025-12-09 20:28:46 -0500

    Added Compiler and Examples

---
+++ caml_comp/_build/default/src/components/eval.ml
@@ -0,0 +1,9 @@
+open Models.Ast
+
+let rec eval : type a. a expr -> a = function
+  | Int n -> n
+  | String s -> s
+  | Add (x, y) -> eval x + eval y
+  | Sub (x, y) -> eval x - eval y
+  | Mult (x, y) -> eval x * eval y
+  | Div (x, y) -> eval x / eval y
