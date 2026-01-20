7b7f8a9b9e00607307f4e7c4958b63266209edb5
Brendan Cron <brendancron@gmail.com>
2026-01-20 11:06:46 -0500

    added several integration tests

--- rust_comp/tests/script_integration.rs
+++ rust_comp/tests/script_integration.rs
@@ -76,8 +76,52 @@ mod script_integration {
     mod vanilla {
         use super::*;
 
+        // Testing Print Utility
         cx_test!(print_hello, "tests/01_vanilla/01_print", "hello");
+
+        // Math and binary operators
         cx_test!(math_math, "tests/01_vanilla/02_math", "math");
-        cx_test!(func_fib, "tests/01_vanilla/03_func", "fib");
+
+        // String functions
+        cx_test!(string_concat, "tests/01_vanilla/03_string", "concat");
+
+        // Variables and Environment
+        cx_test!(
+            environment_variables,
+            "tests/01_vanilla/04_environment",
+            "01_variables"
+        );
+        cx_test!(
+            environment_reassign,
+            "tests/01_vanilla/04_environment",
+            "02_reassign"
+        );
+
+        // Control Flow
+        cx_test!(control_if, "tests/01_vanilla/05_control", "01_if");
+        cx_test!(control_else, "tests/01_vanilla/05_control", "02_else");
+        cx_test!(
+            control_if_else_chain,
+            "tests/01_vanilla/05_control",
+            "03_if_else_chain"
+        );
+
+        // Functions
+        cx_test!(func_greeting, "tests/01_vanilla/06_func", "01_greeting");
+        cx_test!(func_return, "tests/01_vanilla/06_func", "02_return");
+        cx_test!(func_fib, "tests/01_vanilla/06_func", "03_fib");
+        cx_test!(func_closure, "tests/01_vanilla/06_func", "04_closure");
+
+        // Lists
+        cx_test!(list_list, "tests/01_vanilla/07_list", "01_list");
+
+        // Struct
+        cx_test!(struct_struct, "tests/01_vanilla/08_struct", "01_struct");
+
+        // Imports
+        cx_test!(imports_import, "tests/01_vanilla/09_imports", "main");
+
+        // Embed
+        cx_test!(embed_embed, "tests/01_vanilla/10_embed", "embed");
     }
 }
